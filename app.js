c2367cd599fd4e5b8404ea34a4b108d3fe8659c4
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-09-05 20:47:51 +0100

    v1.1

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ var express = require("express"),
     http = require("http").Server(app).listen(80);
 console.log("Server started at port 80");
 app.get("/", function(req, res){
-    res.sendFile(__dirname+"index.html");
+    res.sendFile(__dirname + "/index.html");
 })
