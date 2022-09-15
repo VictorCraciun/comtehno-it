2ec762ddfb50e6ed14a44cb456b457f0f1de6cc9
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-09-15 21:41:02 +0100

    v2.4

--- routes/web/home.js
+++ routes/web/home.js
@@ -1,20 +1,20 @@
 var express = require("express");
+
 var router = express.Router();
 
 router.get("/", function(req, res){
     console.log("Hello I'am an the start here");
-    res.render("index");
+    res.render("home/");
 });
 
 router.get("/home", function(req, res){
     console.log("Hello I'am an home page");
-    res.render("home");
+    res.render("home/home");
 });
 
 router.get("/login", function(req, res){
     console.log("Hello I'am an LogIn");
-    res.render("login");
+    res.render("home/login");
 });
-
 
 module.exports = router;
