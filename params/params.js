f61eb1d8a598ec49d03e130435e1246f3e4e0ec2
VictorCraciun <craciun.victor.tudor@gmail.com>
2022-09-17 12:47:56 +0100

    v2.5

---
+++ params/params.js
@@ -0,0 +1,5 @@
+const DATABASECONNECTION = "mongodb+srv://victor:<Teodora13>@cluster0.ungxvbj.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports = {
+    DATABASECONNECTION
+}
--- routes/web/home.js
+++ routes/web/home.js
@@ -1,5 +1,7 @@
 var express = require("express");
+var passport = require("passport");
 
+var User = require("../../models/user");
 var router = express.Router();
 
 router.get("/", function(req, res){
@@ -16,5 +18,41 @@ router.get("/login", function(req, res){
     console.log("Hello I'am an LogIn");
     res.render("home/login");
 });
+
+router.get("/about", function(req, res){
+    console.log("Hello I'am an About");
+    res.render("home/about");
+});
+
+router.get("/signup", function(req, res){
+    console.log("Hello I'am an SignUP");
+    res.render("home/signup");
+});
+
+router.post("/signup", function(req, res, next){
+    var username =req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({email:email}, function(err, user){
+        if(err){return next(err);}
+        if(user){
+            req.flash("error", "There's already an account with this email");
+            return res.redirect("/signup");
+        }
+
+        var newUser =new User({
+            username:username,
+            password:password,
+            email:email
+        })
+
+        newUser.save(next);
+    });
+}, passport.authenticate("login", {
+    successRedirect:"/",
+    failureRedirect:"/signup",
+    failureFlash:true
+}));
 
 module.exports = router;
